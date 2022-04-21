e4f6be42acd4bbb27c3e0a6ddb932e5b8099a61a
Keila Ramos <keila.ramos@yahoo.com.br>
2022-04-20 21:28:05 -0300

    alteração dia 20 de Abril

--- capitulo5_repeticoes/exercicio5.11_poupanca.py
+++ capitulo5_repeticoes/exercicio5.11_poupanca.py
@@ -4,10 +4,10 @@
 
 depósito = float(input("Depósito inicial: "))
 taxa = float(input("Taxa de juros (Ex.: 3 para 3%): "))
-mês = 1 
-saldo = depósito #acumula valores da taxa a cada repetição
+mês = 1 # mês é um contador, pois o valor a acrescentar é constante *
+saldo = depósito #acumula valores da taxa a cada repetição, valores variam.
 while mês <= 24:
     saldo = saldo + (saldo * (taxa / 100))
     print(f"Saldo do mês {mês} é de R${saldo:5.2f}.")
-    mês = mês + 1
+    mês = mês + 1 #*
 print(f"O ganho obtido com os juros foi de R${saldo - depósito:8.2f}.")
